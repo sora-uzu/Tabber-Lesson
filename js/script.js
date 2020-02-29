211134b4ec3119680715c925c85baeb4e82af335
sora-uzu <naruto07170717@gmail.com>
2020-02-29 14:14:27 +0900

    [FINISH]6-11

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// $(function() {
+ 
+//   $('.menu-trigger').on('click', function() {
+//     $(this).toggleClass('active');
+//     $('#sp-menu').fadeToggle();
+//     return false;
+//   });
+ 
+//  });
+
+$('#tab-contents .tab[id != "tab1"]').hide();
+
+$('#tab-menu a').on('click',function(){
+ $("#tab-contents .tab").hide();
+ $("#tab-menu .active").removeClass("active");
+ $(this).addClass("active");
+ $($(this).attr("href")).show();
+ return false;
+});
+
